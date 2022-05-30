a4ee335964670bd04a38968458edaa306aa27c3a
sachinl0har <sachin.l0har0424@gmail.com>
2022-05-30 22:00:52 +0530

    Partner.html added

--- forms/contactUSForm/contact-us.php
+++ forms/contactUSForm/contact-us.php
@@ -8,16 +8,16 @@
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="../../assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="../../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="../../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="../../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="../../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <link stylesheet href="assets/css/style.css" rel="stylesheet">
-  <link stylesheet href="assets/css/contactUS.css" rel="stylesheet">
+  <link stylesheet href="../../assets/css/style.css" rel="stylesheet">
+  <link stylesheet href="../../assets/css/contactUS.css" rel="stylesheet">
   <title>Contact Us | Nexottel</title>
 </head>
 
@@ -151,23 +151,35 @@
             </div>
           </div>
         </div>
-
         <!-- -------------------------Contact Us form -->
 
         <div class="col-lg-8 mt-5 mt-lg-0">
 
-          <<div class="info">Give Your Feedback!</div>
+          <div class="info">Give Your Feedback!</div>
 
 		<form action="sendEmail.php" method="post" name="form" class="form-box">
-			<label for="name">Name</label><br>
-			<input type="text" name="name" class="inp" placeholder="Enter Your Name" required><br>
-			<label for="email">Email ID</label><br>
-			<input type="email" name="email" class="inp" placeholder="Enter Your Email" required><br>
-			<label for="phone">Phone</label><br>
-			<input type="tel" name="phone" class="inp" placeholder="Enter Your Phone" required><br>
-			<label for="message">Message</label><br>
-			<textarea name="msg" class="msg-box" placeholder="Enter Your Message Here..." required></textarea><br>
-			<input type="submit" name="submit" value="Send" class="sub-btn">
+
+    <div class="row">
+              <div class="col-md-6 form-group">
+              <label for="name">Name</label><br>
+			<input type="text" name="name" class="inp" placeholder="Enter Your Name" required>
+              </div>
+              <div class="col-md-6 form-group mt-3 mt-md-0">
+              <label for="email">Email ID</label><br>
+			<input type="email" name="email" class="inp" placeholder="Enter Your Email" required>
+              </div>
+            </div>
+            <div class="form-group mt-3">
+            <label for="phone">Phone</label><br>
+			<input type="tel" name="phone" class="inp" placeholder="Enter Your Phone" required>
+            </div>
+            <div class="form-group mt-3">
+            <label for="message">Message</label><br>
+			<textarea name="msg" class="msg-box" placeholder="Enter Your Message Here..." required></textarea>
+            </div>
+            <div class="my-3">
+              <input type="submit" name="submit" value="Send" class="sub-btn">
+            </div>
 		</form>
 
         </div>
@@ -232,7 +244,7 @@
         <div class="container">
           <div class="row">
             <div class="col-md-4 col-lg-4 footer-about wow fadeInUp">
-              <img class="logo-footer" src="assets/img/LOGO-1.png" alt="logo-footer" data-at2x="assets/img/LOGO-1.png">
+              <img class="logo-footer" src="../../assets/img/LOGO-1.png" alt="logo-footer" data-at2x="assets/img/LOGO-1.png">
               <p>
                 Hotels worldwide incl. Infos, Ratings and Photos. Make your Hotel Reservation cheap.
                 <br>
@@ -275,7 +287,7 @@
     </footer>
   </section>
   <!-- End Contact Section -->
-  <script src="assets/js/main.js"></script>
+  <script src="../../assets/js/main.js"></script>
   <!-- Optional JavaScript; choose one of the two! -->
   <!-- Option 1: Bootstrap Bundle with Popper -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
