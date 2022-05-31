6150bf2931ec8d53fb87f37b5709b69322824ec3
sachinl0har <sachin.l0har0424@gmail.com>
2022-05-31 15:42:13 +0530

    Career Contact Form Style DONE

--- forms/careerForm/applyCareerForm.php
+++ forms/careerForm/applyCareerForm.php
@@ -2,28 +2,200 @@
 <html>
 
 <head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!------ Include the above in your HEAD tag ---------->
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
+    <link stylesheet href="../../assets/css/style.css" rel="stylesheet">
     <title>Feedback Form</title>
 
 </head>
 
 <body>
-    <div class="main">
-        <form action="sendResumeViaEmail.php" method="post" enctype="multipart/form-data">
-            <input type="text" name="firstname" placeholder="Enter your first name"> <br>
-            <input type="text" name="lastname" placeholder="Enter your last name"> <br>
-            <input type="tel" name="phoneno" placeholder="Enter your contact no"> <br>
-            <input type="email" name="email" placeholder="Enter email"> <br>
-            <input type="text" name="department" placeholder="F & B Service"> <br>
-            <input type="text" name="jobtitle" placeholder="Guest Service Associate - Radisson Hotel Kandla"> <br>
-            <input type="text" name="joblocation" placeholder="Mahabaleshwar"> <br>
-            <input type="file" name="file"> <br>
-            <input type="text" name="message" placeholder="Message"> <br>
 
-            <input type="submit" value="Submit">
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="/">
+        <img src="../../assets/img/LOGO-1.png" alt="" width="120" height="60" class="d-inline-block align-text-top"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="/">HOME</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              OUR SERVICES
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="../../pages/whatWeOfferPages/hotel-management.html">HOTEL MANAGEMENT</a></li>
+              <li><a class="dropdown-item" href="../../pages/whatWeOfferPages/property-development.html"> PROPERTY DEVELOPMENT </a></li>
+              <li><a class="dropdown-item" href="../../pages/whatWeOfferPages/design-management.html">DESIGN MANAGEMENT</a></li>
+              <li><a class="dropdown-item" href="../../pages/whatWeOfferPages/human-resources-training.html">HUMAN RESOURCE AND TRAINING</a></li>
+              <li><a class="dropdown-item" href="../../pages/whatWeOfferPages/marketing-revenue.html">MARKETING & REVENUE MANAGEMENT</a></li>
+              <li><a class="dropdown-item" href="../../pages/whatWeOfferPages/brand-compliance.html">BRAND COMPLIANCE</a></li>
+              <li><a class="dropdown-item" href="../../pages/whatWeOfferPages/technical-services.html">TECHNICAL SERVICES</a></li>
+              <li><a class="dropdown-item" href="../../pages/whatWeOfferPages/finance-and-accounting.html">FINANCE AND ACCOUNTING</a></li>
+              <li><a class="dropdown-item" href="../../pages/whatWeOfferPages/sales.html">SALES</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../../portfolio.html">PORTFOLIO</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../../team-members.html">TEAM</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../../partner.html">PARTNERS</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              MEDIA
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="../../pages/awards&news/awards.html">AWARDS</a></li>
+              <li><a class="dropdown-item" href="../../pages/awards&news/news.html">NEWS</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="../../career.html">CAREER</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../../forms/contactUSForm/contact-us.php">CONTACT US</a>
+          </li>
+        </ul>
+        <!-- <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form> -->
+      </div>
+    </div>
+  </nav>
+
+  <div id="carouselExampleSlidesOnly" class="carousel slide carousel-fade" data-ride="carousel">
+            <div class="carousel-inner" style="max-height:350px !important;">
+              <div class="carousel-item active">
+                <img class="d-block w-100" src="../../assets/img/Slides/slide3.jpg" alt="First slide">
+              </div>
+            </div>
+          </div>
+
+    <div class="main container text-center py-3">
+        <form class="php-email-form" action="sendResumeViaEmail.php" method="post" enctype="multipart/form-data">
+        <div class="row">
+            <div class="col-md-6 form-group">
+            <input class="form-control" type="text" name="firstname" placeholder="Enter your first name">
+            </div>
+            <div class="col-md-6 form-group mt-3 mt-md-0">
+            <input class="form-control" type="text" name="lastname" placeholder="Enter your last name">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 form-group mt-3">
+            <input class="form-control" type="tel" name="phoneno" placeholder="Enter your contact no"> 
+            </div>
+            <div class="col-md-6 form-group mt-3">
+            <input class="form-control" type="email" name="email" placeholder="Enter email">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 form-group mt-3">
+            <input class="form-control" type="text" name="department" placeholder="F & B Service">
+            </div>
+            <div class="col-md-6 form-group mt-3">
+            <input class="form-control" type="text" name="jobtitle" placeholder="Guest Service Associate - Radisson Hotel Kandla">
+            </div>
+        </div>
+            <input class="form-control mt-3" type="text" name="joblocation" placeholder="Mahabaleshwar"> <br>
+            <input class="form-control" type="file" name="file"> <br>
+            <textarea class="form-control" type="text" name="message" placeholder="Message"></textarea> <br>
+
+            <input class="btn btn-dark" type="submit" value="Submit">
         </form>
     </div>
+    <footer>
+      <div class="footer-top">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4 col-lg-4 footer-about wow fadeInUp">
+              <img class="logo-footer" src="../../assets/img/LOGO-1.png" alt="logo-footer" data-at2x="assets/img/LOGO-1.png">
+              <p>
+                Hotels worldwide incl. Infos, Ratings and Photos. Make your Hotel Reservation cheap.
+                <br>
+                The top-rated hotel booking services.
+              </p>
+              <p>
+                <a href="#">Our Team</a>
+              </p>
+            </div>
+            <div class="col-md-4 col-lg-4 offset-lg-1 footer-contact wow fadeInDown">
+              <h3>
+                Contact
+              </h3>
+              <p>
+                <i class="fa fa-map-marker"></i> Corporate office :- Manjari bk, Phursungi Phata, Pune - 412307
+                <br>
+                Branch office :- 396/191 Moo 12, Pratyumnk, Nongprue, Banglamung, Chonburi (Pattaya), Thailand - 20150
+              </p>
+              <p>
+                <i class="fa fa-phone"></i> Phone: +91 9922-972-137
+              </p>
+              <p>
+                <i class="fa fa-envelope"></i> Email: <a href="mailto:hello@domain.com">info@nexottel.com</a>
+              </p>
+            </div>
+            <div class="col-md-4 col-lg-3 footer-social wow fadeInUp">
+              <h3>
+                Follow us
+              </h3>
+              <p>
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-google-plus"></i></a>
+                <a href="#"><i class="fa fa-instagram"></i></a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Back to top button -->
+  <button type="button" class="btn btn-outline-dark btn-floating btn-lg" id="btn-back-to-top">
+    <i class="fa fa-arrow-up"></i>
+  </button>
+  <!-- End Contact Section -->
+  <script src="../../assets/js/main.js"></script>
+  <!-- Optional JavaScript; choose one of the two! -->
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
 </body>
 
 </html>
